05f7f2d0cdd02219bfd8355c4f242fdcb3974c7b
Karol Bakas <karol.bakas@fau.de>
2022-05-13 18:58:41 +0200

    Used ng/mat to create a nice login-card
    Signed-off-by: Karol Bakas <karol.bakas@fau.de>

--- src/digitalIdentity-frontend/src/app/material/material.module.ts
+++ src/digitalIdentity-frontend/src/app/material/material.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 
-const MaterialComponets = [MatButtonModule]
+
+
+const MaterialComponets = [MatButtonModule, MatInputModule, MatCardModule, MatDividerModule]
 
 @NgModule({
   imports: [
