d7b18f886a765acede58cb7b810713607c1b059c
Karol Bakas <karol.bakas@fau.de>
2022-05-14 00:40:45 +0200

    Created a registerpage with angular/materials with basic logic but without a HTTP POST request  (fronted)
    Signed-off-by: Karol Bakas <karol.bakas@fau.de>

--- src/digitalIdentity-frontend/src/app/material/material.module.ts
+++ src/digitalIdentity-frontend/src/app/material/material.module.ts
@@ -3,11 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
 import { MatDividerModule } from '@angular/material/divider';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 
 
-const MaterialComponets = [MatButtonModule, MatInputModule, MatCardModule, MatDividerModule, MatIconModule]
+
+const MaterialComponets = [MatButtonModule, MatInputModule, MatCardModule, MatDividerModule, MatIconModule, MatSelectModule]
 
 @NgModule({
   imports: [
--- src/digitalIdentity-frontend/src/app/register-page/register-page.component.ts
+++ src/digitalIdentity-frontend/src/app/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, isDevMode, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
@@ -6,29 +7,102 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
+  personal_information = this.initPersonalInformaion();
+  formGroup: FormGroup = this.initForm();
 
   constructor() { }
 
   ngOnInit(): void {
+    this.formGroup = this.initForm();
   }
 
-  personal_information = [
-    {
-      description: "name",
-      value: "name",
-      example: "John"
-    },
-    {
-      description: "surname",
-      value: "surname",
-      example: "Doe"
-    },
-    {
-      description: "e-mail",
-      value: "e-mail",
-      example: "john.doe@example.org"
+  initForm(): FormGroup {
+    var formControls: { [id: string]: FormControl } = {}
+
+    this.personal_information.forEach(function (pi) {
+      if (pi.required)
+        formControls[pi.key] = new FormControl('', Validators.required);
+      else
+        formControls[pi.key] = new FormControl('');
+    });
+
+    return new FormGroup(formControls);
+  }
+
+  initPersonalInformaion() {
+    return [
+      {
+        key: "name",
+        label: "Name",
+        placeholder: "John",
+        required: true,
+        value: undefined
+      },
+      {
+        key: "surname",
+        label: "Surname",
+        placeholder: "Doe",
+        required: true,
+        value: undefined
+      },
+      {
+        key: "birthday",
+        label: "Birthday",
+        placeholder: "01.01.1980",
+        required: true,
+        value: undefined
+      },
+      {
+        key: "email",
+        label: "Email",
+        placeholder: "john.doe@example.org",
+        required: true,
+        value: undefined
+      },
+      {
+        key: "user_role",
+        label: "User role",
+        placeholder: "john.doe@example.org",
+        required: true,
+        options: [
+          { value: "basic_employee", viewValue: "Basic employee" },
+          { value: "guest", viewValue: "Guest" },
+          { value: "employee", viewValue: "Employee" },
+          { value: "hr_employee", viewValue: "HR Employee" }
+          // admin role exists but can not be added via the web app. 
+        ],
+        value: undefined
+      },
+      {
+        key: "company",
+        label: "Company",
+        placeholder: "Friedrich-Alexander Universität",
+        required: true,
+        value: undefined
+      },
+      {
+        key: "team",
+        label: "Team",
+        placeholder: "Lehrstuhl 4",
+        required: false,
+        value: undefined
+      }
+    ];
+  }
+
+  inDevelopment(): boolean {
+    return isDevMode();
+  }
+
+  registerProcess(): void {
+    if (this.formGroup.valid) {
+      var formGroup = this.formGroup
+      var personal_information = this.personal_information
+
+      this.personal_information.forEach(function (pi, index: number) {
+        personal_information[index].value = formGroup.value[pi.key]
+      })
     }
+  }
 
-
-  ]
 }
