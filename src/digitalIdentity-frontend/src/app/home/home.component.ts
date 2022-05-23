518c438bf3ec40dfdf2b6ffccdeb26ba7a579af6
Steve <steve.pegouen@fau.de>
2022-05-23 09:19:45 +0200

    dashboard
    Signed-off-by: Steve <steve.pegouen@fau.de>

--- src/digitalIdentity-frontend/src/app/home/home.component.ts
+++ src/digitalIdentity-frontend/src/app/home/home.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+//my importation
 
-@Component({ templateUrl: 'home.component.html' })
-export class HomeComponent {
+@Component({
+  templateUrl: 'home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+  //what l added
+
+  logo1!: string;
+  logo2!: string;
+
   clicked(str:string) : void {
-    console.log("goto " + str)
+    console.log("goto " + str);
+  }
+
+  ngOnInit() {
+    this.logo1 = `./images/DIdentity.png`;
+    this.logo2 = `../images/adorsys.png`;
   }
 }
