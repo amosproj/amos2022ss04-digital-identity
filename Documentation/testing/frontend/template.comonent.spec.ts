94fc9ef95463e695508a5a59f0ed2d09b3f4acd2
Karol Bakas <karol.bakas@fau.de>
2022-05-24 13:30:18 +0200

    Added a template testfile
    created a template file based on the FrontendTesting.md
    
    Signed-off-by: Karol Bakas <karol.bakas@fau.de>

---
+++ Documentation/testing/frontend/template.comonent.spec.ts
@@ -0,0 +1,26 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MyComponent } from './myComponent.component'; // TODO: adjust 
+import { DebugElement } from '@angular/core';
+
+
+describe('MyComponent', () => {
+  let component: MyComponent;
+  let fixture: ComponentFixture<MyComponent>;
+  let de: DebugElement
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ... ], // TODO: adjust
+      imports:[...] // TODO: adjust
+    })
+      .compileComponents(); // compiles the components html and css
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MyComponent); // fixture is the test envoirment for the component
+    component = fixture.componentInstance; // the component itself
+    de = fixture.debugElement; // the rendered component
+    
+    fixture.detectChanges();
+  });
+});
