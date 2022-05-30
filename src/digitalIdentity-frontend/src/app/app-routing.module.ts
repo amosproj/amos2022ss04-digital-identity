2ae133ddc544ebc9158cc93ab3340acc6092175e
Steve <steve.pegouen@fau.de>
2022-05-30 05:26:31 +0200

    creation of the navigation bar
    Signed-off-by: Steve <steve.pegouen@fau.de>

--- src/digitalIdentity-frontend/src/app/app-routing.module.ts
+++ src/digitalIdentity-frontend/src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import { AppComponent } from './app.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { HomeComponent } from './home/home.component';
-
 import {SettingsComponent} from './settings/settings.component';
 import { DIOverviewComponent } from './DI-Overview/DI-Overview.component';
+import { SchemaOverviewComponent } from './schema-overview/schema-overview.component';
 // import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
@@ -17,6 +17,7 @@ const appRoutes: Routes = [
   // { path: 'home', redirectTo: '', pathMatch: 'full' },
   { path: '', component: HomeComponent },
   { path: 'settings', component: SettingsComponent },
+  { path: 'schema-overview', component: SchemaOverviewComponent },
   // { path: '',   redirectTo: '/login', pathMatch: 'full' }
   // { path: '**', component: PageNotFoundComponent }
 ];
