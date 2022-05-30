02bf37a7dc48de79bef41c318c7a9e045c8f2647
Jannik Kiesel <jannik.kiesel@fau.de>
2022-05-30 18:40:54 +0200

    #34 Added some example test cases
    Signed-off-by: Jannik Kiesel <jannik.kiesel@fau.de>

--- src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/controller/AuthentificationControllerTests.java
+++ src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/controller/AuthentificationControllerTests.java
@@ -1,29 +1,19 @@
 package didentity.amos.digitalIdentity.controller;
 
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 
 import didentity.amos.digitalIdentity.enums.UserRole;
 import didentity.amos.digitalIdentity.model.User;
-import didentity.amos.digitalIdentity.repository.UserRepository;
 
-@Controller
+
 public class AuthentificationControllerTests {
 
-    @Autowired
-    private AuthenticationController authenticationController;
-
-    @Autowired
-    private UserRepository userRepository;
 
     private static User user = new User();
 
     @BeforeAll
-    public void setup() {
+    public static void setup() {
         String email = "test@test.de";
         String password = "password";
 
@@ -36,28 +26,12 @@ public class AuthentificationControllerTests {
         user.setSurname("test");
         user.setTeam("team");
         user.setUserRole(UserRole.EMPLOYEE);
-
-        userRepository.save(user);
     }
     
-    @Test
-    public void testLogin(){
-        // Arrange
-        String email = "test@test.de";
-        String password = "password";
-
-        String expected = "\"success\"";
-
-        // Act
-        String result = authenticationController.login(email, password);
-        
-        // Assert
-        Assertions.assertEquals(expected, result);
-    }
 
     @AfterAll
-    public void teardown() {
-        // userRepository.delete(user);
+    public static void teardown() {
+        
     }
     
 }
--- src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/controller/ConnectionControllerTests.java
+++ src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/controller/ConnectionControllerTests.java
@@ -1,25 +1,80 @@
 package didentity.amos.digitalIdentity.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 
+
 public class ConnectionControllerTests {
-    @Test
-    void testAuthentification() {
 
+
+    @Test
+    void testAuthentification_withPassing_shouldBeAccepted() {
+        // Arrange
+        boolean expected = true;
+        ConnectionController connectionController = new ConnectionController();
+        String authorization = "passing";
+
+        // Act
+        boolean result = connectionController.authentification(authorization);
+        
+        //Assert
+        assertEquals(expected, result);
     }
 
     @Test
-    void testGetAll() {
+    void testAuthentification_withAdmin_shouldBeAccepted() {
+        // Arrange
+        boolean expected = true;
+        ConnectionController connectionController = new ConnectionController();
+        String authorization = "admin";
 
+        // Act
+        boolean result = connectionController.authentification(authorization);
+        
+        //Assert
+        assertEquals(expected, result);
     }
 
     @Test
-    void testGetConnection() {
+    void testAuthentification_withoutFalseAuthorization_shouldBeDenied() {
+        // Arrange
+        boolean expected = false;
+        ConnectionController connectionController = new ConnectionController();
+        String authorization = "hacker";
 
+        // Act
+        boolean result = connectionController.authentification(authorization);
+        
+        //Assert
+        assertEquals(expected, result);
     }
 
+    @Test
+    void testAuthentification_withEmptyAuthorization_shouldBeDenied() {
+        // Arrange
+        boolean expected = false;
+        ConnectionController connectionController = new ConnectionController();
+        String authorization = "";
+
+        // Act
+        boolean result = connectionController.authentification(authorization);
+        
+        //Assert
+        assertEquals(expected, result);
+    }
+    
     @Test
     void testUnavailable() {
+        // Arrange
+        boolean expected = true;
+        ConnectionController connectionController = new ConnectionController();
 
+        // Act
+        boolean result = connectionController.unavailable();
+        
+        //Assert
+        assertEquals(expected, result);
     }
+    
 }
