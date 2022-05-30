b69122d1dcdf7c7257bb6ce3115f029623c2e2ab
Jannik Kiesel <jannik.kiesel@fau.de>
2022-05-30 10:15:35 +0200

    Changes of review
    Signed-off-by: Jannik Kiesel <jannik.kiesel@fau.de>

--- src/digitalIdentity-backend/src/main/java/didentity/QrGenerator/QrGenerator.java
+++ src/digitalIdentity-backend/src/main/java/didentity/QrGenerator/QrGenerator.java
@@ -4,33 +4,20 @@ import com.google.zxing.BarcodeFormat;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
-import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.File;
-import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
 public class QrGenerator {
 
-    public BufferedImage generateQRCodeImage(String url) throws Exception {
+    public File generateQRCodeImage(String url, String name) throws Exception {
         QRCodeWriter barcodeWriter = new QRCodeWriter();
+        
         BitMatrix bitMatrix = barcodeWriter.encode(url, BarcodeFormat.QR_CODE, 200, 200);
 
-        System.out.print(MatrixToImageWriter.toBufferedImage(bitMatrix));
-
-        return MatrixToImageWriter.toBufferedImage(bitMatrix);
-    }
-
-    public File getImageAsJpg (RenderedImage image, String name) throws IOException {
-        File outputfile = new File(name + ".jpg");
-        ImageIO.write(image, "jpg", outputfile);
-        return outputfile;
-    }
-
-    public File getImageAsPng (RenderedImage image, String name) throws IOException {
         File outputfile = new File(name + ".png");
-        ImageIO.write(image, "png", outputfile);
+        ImageIO.write(MatrixToImageWriter.toBufferedImage(bitMatrix), "png", outputfile);
+
         return outputfile;
     }
 
