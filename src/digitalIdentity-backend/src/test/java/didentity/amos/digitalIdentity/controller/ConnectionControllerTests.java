ec6d38a9b821be36465ab57d7b09beb2e2c58fdf
Jannik Kiesel <jannik.kiesel@fau.de>
2022-05-30 16:11:58 +0200

    #34 Added test classes
    Signed-off-by: Jannik Kiesel <jannik.kiesel@fau.de>

---
+++ src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/controller/ConnectionControllerTests.java
@@ -0,0 +1,25 @@
+package didentity.amos.digitalIdentity.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class ConnectionControllerTests {
+    @Test
+    void testAuthentification() {
+
+    }
+
+    @Test
+    void testGetAll() {
+
+    }
+
+    @Test
+    void testGetConnection() {
+
+    }
+
+    @Test
+    void testUnavailable() {
+
+    }
+}
---
+++ src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/enums/UserRoleTest.java
@@ -0,0 +1,20 @@
+package didentity.amos.digitalIdentity.enums;
+
+import org.junit.jupiter.api.Test;
+
+public class UserRoleTest {
+    @Test
+    void testFromString() {
+
+    }
+
+    @Test
+    void testValueOf() {
+
+    }
+
+    @Test
+    void testValues() {
+
+    }
+}
