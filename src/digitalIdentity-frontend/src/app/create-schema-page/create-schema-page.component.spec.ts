f24b83792e12de853f5efb7c426a7456460e5255
Valentin Bräutigam <valentin.braeutigam@fau.de>
2022-05-31 10:19:57 +0200

    minor changes in testing modules
    Signed-off-by: Valentin Bräutigam <valentin.braeutigam@fau.de>

--- src/digitalIdentity-frontend/src/app/create-schema-page/create-schema-page.component.spec.ts
+++ src/digitalIdentity-frontend/src/app/create-schema-page/create-schema-page.component.spec.ts
@@ -1,19 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 
 import { CreateSchemaPageComponent } from './create-schema-page.component';
 
 describe('CreateSchemaPageComponent', () => {
   let component: CreateSchemaPageComponent;
   let fixture: ComponentFixture<CreateSchemaPageComponent>;
-
+  let fb: FormBuilder;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateSchemaPageComponent ]
+      declarations: [ CreateSchemaPageComponent ],
+      providers: [{ provide: FormBuilder, useValue: {}}]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
+    fb = new FormBuilder();
     fixture = TestBed.createComponent(CreateSchemaPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
