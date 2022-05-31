2a13b4cccc0c7b4e4b4094040c9f65d76780070b
Jannik Kiesel <jannik.kiesel@fau.de>
2022-05-31 23:50:06 +0200

    gitignoretest
    Signed-off-by: Jannik Kiesel <jannik.kiesel@fau.de>

--- src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/Credentials.java
+++ src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/Credentials.java
@@ -3,7 +3,7 @@ package didentity.amos.digitalIdentity;
 public class Credentials {
     
         // Credentials for Lissi API, DO NOT PUBLISH
-        String credentials= "Bitte selbst eingeben";
+        String credentials= "Bitte selbst eingeben!";
 
         public String getCredentials() {
             return credentials;
