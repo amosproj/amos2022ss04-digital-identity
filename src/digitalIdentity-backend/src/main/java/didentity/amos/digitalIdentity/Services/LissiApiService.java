ce37697091ecd527e27069921874f6fe512c93ed
Karol Bakas <karol.bakas@fau.de>
2022-06-01 18:06:03 +0200

    added clientid and clientsecret to app.prop
    Signed-off-by: Karol Bakas <karol.bakas@fau.de>

--- src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/Services/LissiApiService.java
+++ src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/Services/LissiApiService.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,19 +19,21 @@ import didentity.amos.digitalIdentity.model.CreateConnectionResponse;
 
 @Service
 public class LissiApiService {
-    
+
     private final RestTemplate restTemplate;
 
-    // TODO Credentials sollen in Variable abgespeichert werden und nicht auf git gepusht werden
-    private String credentials = "";
+    @Value("${lissi.client.id}")
+    private String clientID;
+    @Value("${lissi.client.secret}")
+    private String clientSecret;
 
     public LissiApiService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
     /**
-	 * Creates new connection and returns invitation url.
-	 */
+     * Creates new connection and returns invitation url.
+     */
     public String createConnectionInvitation(String alias) {
         String baseUrl = "https://onboardingad.ddns.net";
         String endpoint = "/ctrl/api/v1.0/connections/create-invitation";
@@ -45,10 +48,11 @@ public class LissiApiService {
         headers.add("alias", alias);
 
         // build the request
-		HttpEntity<String> entity = new HttpEntity<String>(headers);
+        HttpEntity<String> entity = new HttpEntity<String>(headers);
 
         // send POST request
-        ResponseEntity<CreateConnectionResponse> response = this.restTemplate.postForEntity(url, entity, CreateConnectionResponse.class);
+        ResponseEntity<CreateConnectionResponse> response = this.restTemplate.postForEntity(url, entity,
+                CreateConnectionResponse.class);
 
         // check response status code
         if (response.getStatusCode() == HttpStatus.OK) {
@@ -59,24 +63,27 @@ public class LissiApiService {
     }
 
     private String getOAuth2Auhotization() {
-        String bodyAsString = "grant_type=client_credentials&scope=openid&client_id=springboot-client&client_secret=" + credentials;
+        String bodyAsString = "grant_type=client_credentials&scope=openid"
+                + "&client_id=" + clientID
+                + "&client_secret=" + clientSecret;
         String access_token_url = "https://onboardingad.ddns.net/auth/realms/lissi-cloud/protocol/openid-connect/token";
 
         Map<String, String> body = new HashMap<>();
         body.put("grant_type", "client_credentials");
         body.put("scope", "openid");
-        body.put("client_id", "springboot-client");
-        body.put("client_secret", credentials);
+        body.put("client_id", clientID);
+        body.put("client_secret", clientSecret);
 
-		HttpHeaders headers = new HttpHeaders();
+        HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
 
-		HttpEntity<String> request = new HttpEntity<String>(bodyAsString, headers);
+        HttpEntity<String> request = new HttpEntity<String>(bodyAsString, headers);
 
-        ResponseEntity<Accesstoken> response = this.restTemplate.postForEntity(access_token_url, request, Accesstoken.class);
+        ResponseEntity<Accesstoken> response = this.restTemplate.postForEntity(access_token_url, request,
+                Accesstoken.class);
 
-		String token = "Bearer " + response.getBody().getAccessToken();
+        String token = "Bearer " + response.getBody().getAccessToken();
 
         return token;
     }
