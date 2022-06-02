4e36a1bd268662ab7d9b12f8fa5a04847079bb46
Karol Bakas <karol.bakas@fau.de>
2022-06-02 18:21:42 +0200

    set up tests
    Signed-off-by: Karol Bakas <karol.bakas@fau.de>

--- src/digitalIdentity-frontend/src/app/error-pages/error-page/error-page.component.spec.ts
+++ src/digitalIdentity-frontend/src/app/error-pages/error-page/error-page.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ErrorPageComponent } from './error-page.component';
 
@@ -8,9 +9,9 @@ describe('ErrorPageComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ErrorPageComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule],
+      declarations: [ErrorPageComponent],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -21,5 +22,15 @@ describe('ErrorPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  //TODO
+  it('should render/contain only one div with the class error-block', () => {});
+  //TODO
+  it('Routing to error/400 will show the correct error number', () => {
+    // these should be tested with a loop [400, 401, 403, 404, 408, 418, 500, 503]
+  });
+  it('Routing to error/wilderpath or error/111 result in the 404 page', () => {
+    // these should be tested with a loop [400, 401, 403, 404, 408, 418, 500, 503]
   });
 });
