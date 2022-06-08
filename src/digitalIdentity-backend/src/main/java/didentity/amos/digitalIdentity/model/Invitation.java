7213565b684d97a41526db2e301d88c3f3709229
Karol Bakas <karol.bakas@fau.de>
2022-06-08 17:59:15 +0200

    #121: autoformating
    * Accesstoken.java
    * Invitation.java
    * CreateConnectionResponse.java
    
    Signed-off-by: Karol Bakas <karol.bakas@fau.de>

--- src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/model/Invitation.java
+++ src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/model/Invitation.java
@@ -13,125 +13,125 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
-"@id",
-"@type",
-"did",
-"imageUrl",
-"label",
-"recipientKeys",
-"routingKeys",
-"serviceEndpoint"
+        "@id",
+        "@type",
+        "did",
+        "imageUrl",
+        "label",
+        "recipientKeys",
+        "routingKeys",
+        "serviceEndpoint"
 })
 @Generated("jsonschema2pojo")
 public class Invitation {
 
-@JsonProperty("@id")
-private String id;
-@JsonProperty("@type")
-private String type;
-@JsonProperty("did")
-private String did;
-@JsonProperty("imageUrl")
-private String imageUrl;
-@JsonProperty("label")
-private String label;
-@JsonProperty("recipientKeys")
-private List<String> recipientKeys = null;
-@JsonProperty("routingKeys")
-private List<String> routingKeys = null;
-@JsonProperty("serviceEndpoint")
-private String serviceEndpoint;
-@JsonIgnore
-private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+    @JsonProperty("@id")
+    private String id;
+    @JsonProperty("@type")
+    private String type;
+    @JsonProperty("did")
+    private String did;
+    @JsonProperty("imageUrl")
+    private String imageUrl;
+    @JsonProperty("label")
+    private String label;
+    @JsonProperty("recipientKeys")
+    private List<String> recipientKeys = null;
+    @JsonProperty("routingKeys")
+    private List<String> routingKeys = null;
+    @JsonProperty("serviceEndpoint")
+    private String serviceEndpoint;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
 
-@JsonProperty("@id")
-public String getId() {
-return id;
-}
+    @JsonProperty("@id")
+    public String getId() {
+        return id;
+    }
 
-@JsonProperty("@id")
-public void setId(String id) {
-this.id = id;
-}
+    @JsonProperty("@id")
+    public void setId(String id) {
+        this.id = id;
+    }
 
-@JsonProperty("@type")
-public String getType() {
-return type;
-}
+    @JsonProperty("@type")
+    public String getType() {
+        return type;
+    }
 
-@JsonProperty("@type")
-public void setType(String type) {
-this.type = type;
-}
+    @JsonProperty("@type")
+    public void setType(String type) {
+        this.type = type;
+    }
 
-@JsonProperty("did")
-public String getDid() {
-return did;
-}
+    @JsonProperty("did")
+    public String getDid() {
+        return did;
+    }
 
-@JsonProperty("did")
-public void setDid(String did) {
-this.did = did;
-}
+    @JsonProperty("did")
+    public void setDid(String did) {
+        this.did = did;
+    }
 
-@JsonProperty("imageUrl")
-public String getImageUrl() {
-return imageUrl;
-}
+    @JsonProperty("imageUrl")
+    public String getImageUrl() {
+        return imageUrl;
+    }
 
-@JsonProperty("imageUrl")
-public void setImageUrl(String imageUrl) {
-this.imageUrl = imageUrl;
-}
+    @JsonProperty("imageUrl")
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 
-@JsonProperty("label")
-public String getLabel() {
-return label;
-}
+    @JsonProperty("label")
+    public String getLabel() {
+        return label;
+    }
 
-@JsonProperty("label")
-public void setLabel(String label) {
-this.label = label;
-}
+    @JsonProperty("label")
+    public void setLabel(String label) {
+        this.label = label;
+    }
 
-@JsonProperty("recipientKeys")
-public List<String> getRecipientKeys() {
-return recipientKeys;
-}
+    @JsonProperty("recipientKeys")
+    public List<String> getRecipientKeys() {
+        return recipientKeys;
+    }
 
-@JsonProperty("recipientKeys")
-public void setRecipientKeys(List<String> recipientKeys) {
-this.recipientKeys = recipientKeys;
-}
+    @JsonProperty("recipientKeys")
+    public void setRecipientKeys(List<String> recipientKeys) {
+        this.recipientKeys = recipientKeys;
+    }
 
-@JsonProperty("routingKeys")
-public List<String> getRoutingKeys() {
-return routingKeys;
-}
+    @JsonProperty("routingKeys")
+    public List<String> getRoutingKeys() {
+        return routingKeys;
+    }
 
-@JsonProperty("routingKeys")
-public void setRoutingKeys(List<String> routingKeys) {
-this.routingKeys = routingKeys;
-}
+    @JsonProperty("routingKeys")
+    public void setRoutingKeys(List<String> routingKeys) {
+        this.routingKeys = routingKeys;
+    }
 
-@JsonProperty("serviceEndpoint")
-public String getServiceEndpoint() {
-return serviceEndpoint;
-}
+    @JsonProperty("serviceEndpoint")
+    public String getServiceEndpoint() {
+        return serviceEndpoint;
+    }
 
-@JsonProperty("serviceEndpoint")
-public void setServiceEndpoint(String serviceEndpoint) {
-this.serviceEndpoint = serviceEndpoint;
-}
+    @JsonProperty("serviceEndpoint")
+    public void setServiceEndpoint(String serviceEndpoint) {
+        this.serviceEndpoint = serviceEndpoint;
+    }
 
-@JsonAnyGetter
-public Map<String, Object> getAdditionalProperties() {
-return this.additionalProperties;
-}
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
 
-@JsonAnySetter
-public void setAdditionalProperty(String name, Object value) {
-this.additionalProperties.put(name, value);
-}
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
 
 }
