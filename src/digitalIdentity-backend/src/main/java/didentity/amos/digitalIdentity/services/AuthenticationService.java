135dde99b2144ac89c0cd71665e018525ff852c6
Jean28518 <jean-frederic.vog.vogelbacher@fau.de>
2022-06-11 12:26:23 +0200

    Refactor authentication controller
    Signed-off-by: Jean28518 <jean-frederic.vog.vogelbacher@fau.de>

--- src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/services/AuthenticationService.java
+++ src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/services/AuthenticationService.java
@@ -1,10 +1,17 @@
 package didentity.amos.digitalIdentity.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import didentity.amos.digitalIdentity.model.User;
+import didentity.amos.digitalIdentity.repository.UserRepository;
+
 @Service
 public class AuthenticationService {
+
+    @Autowired
+    private UserRepository userRepository;
 
     private ResponseEntity<String> response401;
     private ResponseEntity<String> response403;
@@ -38,6 +45,29 @@ public class AuthenticationService {
     private boolean validation(String token) {
         return token.equalsIgnoreCase("passing") == true
                 || token.equalsIgnoreCase("admin") == true;
+    }
+
+    public ResponseEntity<String> login(
+            String email,
+            String password) {
+
+        if (email == null || email == "") {
+            return ResponseEntity.status(400).body("\"Bad request. Email is empty.\"");
+        }
+
+        if (password == null || password == "") {
+            return ResponseEntity.status(400).body("\"Bad request. Password is empty.\"");
+        }
+
+        // TODO Jannik: findAll() ist ziemlich inperformant; Ich wusste leider nicht wie
+        // man e_mail und password direkt im Repository abfragen kann
+        Iterable<User> users = userRepository.findAll();
+        for (User user : users) {
+            if (user.getEmail().equals(email) && user.getPassword().equals(password)) {
+                return ResponseEntity.status(200).body("\"Login successful.\"");
+            }
+        }
+        return ResponseEntity.status(200).body("\"Password and username do not match.\"");
     }
 
 }
--- src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/services/DIConnectionService.java
+++ src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/services/DIConnectionService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import didentity.amos.digitalIdentity.enums.UserRole;
 import didentity.amos.digitalIdentity.model.User;
 import didentity.amos.digitalIdentity.repository.UserRepository;
 
@@ -18,6 +19,9 @@ public class DIConnectionService {
 
     @Autowired
     private LissiApiService lissiApiService;
+
+    @Autowired
+    private MailService mailService;
 
     public String invite(String alias) {
         return lissiApiService.createConnectionInvitation(alias);
@@ -57,5 +61,111 @@ public class DIConnectionService {
 
         return ResponseEntity.status(200).body(json_string);
     }
+
+    public ResponseEntity<String> register(
+            String name,
+            String surname,
+            String email,
+            String user_role) {
+
+        User user = new User();
+        user.setName(name);
+        user.setSurname(surname);
+        user.setEmail(email);
+
+        // TODO: create onetime password
+        user.setPassword("test");
+
+        if (user_role != null && user_role != "") {
+            switch (user_role) {
+                case "admin":
+                    user.setUserRole(UserRole.fromString("ROLE_ADMIN"));
+                    break;
+                case "employee":
+                    user.setUserRole(UserRole.fromString("ROLE_EMPLOYEE"));
+                    break;
+                case "hr_employee":
+                    user.setUserRole(UserRole.fromString("ROLE_HR_EMPLOYEE"));
+                    break;
+                case "guest":
+                    user.setUserRole(UserRole.fromString("ROLE_GUEST"));
+                    break;
+                default:
+                    return ResponseEntity.status(500).body("\"User role not recognized.\"");
+            }
+        }
+
+        userRepository.save(user);
+
+        try {
+            String invitationUrl = lissiApiService.createConnectionInvitation(email);
+            String mailSuccess = mailService.sendInvitation(email, invitationUrl);
+            if (!mailSuccess.equals("success")) {
+                return ResponseEntity.status(500).body("\"Mail couldn't be sent! Error: " + mailSuccess + "\"");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500)
+                    .body("\"Invitation in Lissi could not be created! Error: " + e.toString() + "\"");
+        }
+        return ResponseEntity.status(200).body("\"Successful creation of the digital identity.\"");
+
+        // TODO:
+        // error 400: if email address is already in use for another connection/DI (this
+        // test has to be implemented first!)
+    }
+
+    public ResponseEntity<String> update(
+            Integer id,
+            String name,
+            String surname,
+            String email,
+            String user_role) {
+
+        LinkedList<Integer> ids = new LinkedList<Integer>();
+        ids.add(id);
+        // TODO: maybe use findById instead? This would skip all the Iterator stuff
+        Iterable<User> DIs = userRepository.findAllById(ids);
+
+        Iterator<User> diIterator = DIs.iterator();
+        if (!diIterator.hasNext()) {
+            // TODO: might need a change. Otherwise you can fish for a valid id.
+            return ResponseEntity.status(400).body("\"No DI with this id was found.\"");
+        }
+        User firstDI = diIterator.next();
+
+        if (name != null && name != "") {
+            firstDI.setName(name);
+        }
+        if (surname != null && surname != "") {
+            firstDI.setSurname(surname);
+        }
+        if (email != null && surname != "") {
+            firstDI.setEmail(email);
+        }
+
+        if (user_role != null && user_role != "") {
+            switch (user_role) {
+                case "admin":
+                    firstDI.setUserRole(UserRole.fromString("ROLE_ADMIN"));
+                    break;
+                case "employee":
+                    firstDI.setUserRole(UserRole.fromString("ROLE_EMPLOYEE"));
+                    break;
+                case "hr_employee":
+                    firstDI.setUserRole(UserRole.fromString("ROLE_HR_EMPLOYEE"));
+                    break;
+                case "guest":
+                    firstDI.setUserRole(UserRole.fromString("ROLE_GUEST"));
+                    break;
+                default:
+                    return ResponseEntity.status(500).body("\"User role not recognized.\"");
+            }
+        }
+
+        userRepository.save(firstDI);
+        return ResponseEntity.status(200).body(firstDI.toString());
+    }
+
 
 }
