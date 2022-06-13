07a24b61fafeaf048e9c568b1661d0bcf7b0d8ba
Karol Bakas <karol.bakas@fau.de>
2022-06-13 12:55:52 +0200

    #111 refactored lissiApi.createSchema into a new HttpService
    Signed-off-by: Karol Bakas <karol.bakas@fau.de>

--- src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/services/LissiApiService.java
+++ src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/services/LissiApiService.java
@@ -1,13 +1,12 @@
 package didentity.amos.digitalIdentity.services;
 
 import java.io.File;
-import java.nio.file.Files;
-import java.util.Arrays;
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.util.Pair;
 import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -20,7 +19,6 @@ import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
-import didentity.amos.digitalIdentity.messages.answers.Accesstoken;
 import didentity.amos.digitalIdentity.messages.responses.CreateConnectionResponse;
 
 @Service
@@ -28,37 +26,14 @@ public class LissiApiService {
 
     private final RestTemplate restTemplate;
 
+    @Autowired
+    private HttpService httpService;
+
     @Value("${lissi.api.url}")
     private String baseUrl;
 
-    @Value("${lissi.auth.url}")
-    private String authentificationUrl;
-    @Value("${lissi.auth.client.id}")
-    private String clientID;
-    @Value("${lissi.auth.client.secret}")
-    private String clientSecret;
-
     public LissiApiService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
-    }
-
-    private String getOAuth2Authorization() {
-        String bodyAsString = "grant_type=client_credentials&scope=openid"
-                + "&client_id=" + clientID
-                + "&client_secret=" + clientSecret;
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-
-        HttpEntity<String> request = new HttpEntity<String>(bodyAsString, headers);
-
-        ResponseEntity<Accesstoken> response = this.restTemplate.postForEntity(authentificationUrl, request,
-                Accesstoken.class);
-
-        String token = "Bearer " + response.getBody().getAccessToken();
-
-        return token;
     }
 
     /**
@@ -68,12 +43,9 @@ public class LissiApiService {
         String url = baseUrl + "/ctrl/api/v1.0/connections/create-invitation";
 
         // build headers
-        HttpHeaders headers = new HttpHeaders();
-
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-        headers.add("Authorization", getOAuth2Authorization());
-        headers.add("alias", alias);
+        HttpHeaders headers = httpService.createHttpHeader(
+                MediaType.APPLICATION_JSON,
+                Collections.singletonList(MediaType.APPLICATION_JSON));
 
         // build the request
         HttpEntity<String> entity = new HttpEntity<String>(headers);
@@ -95,19 +67,49 @@ public class LissiApiService {
      *
      * @param attributes should be a String in form: ["attrib1", "attrib2"]
      */
-    public boolean createSchema(String alias, String imageUri, String version, String attributes) {
+    @SuppressWarnings("unchecked") // TODO: if someone wants to bother with generic arrays, feel free :)
+    public boolean createSchema(String alias, String imageUri, String version, String attributes, File file) {
+
         String baseUrl = "https://onboardingad.ddns.net";
         String endpoint = "/ctrl/api/v1.0/schemas/create";
         String url = baseUrl + endpoint;
-        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = null;
-        String response = "empty";
-        HttpStatus httpStatus = HttpStatus.CREATED;
 
         // build headers
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-        headers.add("Authorization", getOAuth2Authorization());
-        headers.add("alias", alias);
+        HttpHeaders headers = httpService.createHttpHeader(MediaType.MULTIPART_FORM_DATA);
+
+        // build body
+
+        Pair<String, File>[] fileParams = new Pair[] { Pair.of("image", file) };
+        LinkedMultiValueMap<String, Object> body = httpService.createHttpBody(
+                fileParams,
+                Pair.of("alias", alias),
+                Pair.of("imageUri", imageUri),
+                Pair.of("version", version),
+                Pair.of("attributes", attributes));
+        if (body == null) {
+            return false;
+        }
+
+        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+        String response;
+        try {
+            response = restTemplate.postForObject(url, requestEntity, String.class);
+        } catch (HttpStatusCodeException e) {
+            HttpStatus httpStatus = HttpStatus.valueOf(e.getStatusCode().value());
+            response = e.getResponseBodyAsString();
+            System.err.println(httpStatus);
+            System.err.println("response: ");
+            System.err.println(response);
+            return httpStatus.equals(HttpStatus.CREATED);
+        }
+        return true;
+    }
+
+    public boolean createCredential(String alias, String comment, String imageUri, String schemaId) {
+        String url = baseUrl + "/ctrl/api/v1.0/credential-definitions/create";
+        String revocable = "false";
+
+        HttpHeaders headers = httpService.createHttpHeader(MediaType.MULTIPART_FORM_DATA);
 
         // build body
         LinkedMultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
@@ -125,32 +127,7 @@ public class LissiApiService {
                 .filename("dummy")
                 .build();
         fileMap.add(HttpHeaders.CONTENT_DISPOSITION, contentDisposition.toString());
-        try {
 
-            // read image and write it into the body
-            File file = new ClassPathResource("img/logo.png").getFile();
-            byte[] content = Files.readAllBytes(file.toPath());
-            HttpEntity<byte[]> fileEntity = new HttpEntity<>(content, fileMap);
-            body.add("image", fileEntity);
-
-            requestEntity = new HttpEntity<>(body, headers);
-            response = restTemplate.postForObject(url, requestEntity, String.class);
-        } catch (HttpStatusCodeException e) {
-            httpStatus = HttpStatus.valueOf(e.getStatusCode().value());
-            response = e.getResponseBodyAsString();
-        } catch (Exception e) {
-            httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
-            e.printStackTrace();
-            response = e.getMessage();
-        } finally {
-            System.err.println(httpStatus);
-            System.err.println("response: ");
-            System.err.println(response);
-        }
-        return httpStatus.equals(HttpStatus.CREATED);
-    }
-
-    public boolean createCredential(String alias, String imageUri, String schemaId) {
         return false;
     }
 }
