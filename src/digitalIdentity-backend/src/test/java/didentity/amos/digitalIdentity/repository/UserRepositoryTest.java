20dae5d26cfe3f79b79084be07e5eacfeb9e571b
Karol Bakas <karol.bakas@fau.de>
2022-06-14 16:10:16 +0200

    setup for testing database
    Signed-off-by: Karol Bakas <karol.bakas@fau.de>

--- src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/repository/UserRepositoryTest.java
+++ src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/repository/UserRepositoryTest.java
@@ -1,4 +1,33 @@
 package didentity.amos.digitalIdentity.repository;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import didentity.amos.digitalIdentity.model.User;
+
 public class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // test DataBase creation
+    @Test
+    void itShouldFindACreatedUser() {
+        // given
+        String email = "test@mail.org";
+        User user = new User();
+        user.setEmail(email);
+        userRepository.save(user);
+
+        // when
+        Optional<User> optional = userRepository.findByEmail(email);
+
+        // then
+        assertTrue(optional.isPresent());
+    }
+
 }
