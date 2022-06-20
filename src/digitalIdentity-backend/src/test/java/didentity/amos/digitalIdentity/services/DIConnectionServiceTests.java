e04af06731bf61058af0601e85ae807eb3b8ab95
Jean28518 <jean-frederic.vog.vogelbacher@fau.de>
2022-06-20 16:40:26 +0200

    Redefine tests with mockito
    Signed-off-by: Jean28518 <jean-frederic.vog.vogelbacher@fau.de>

--- src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/services/DIConnectionServiceTests.java
+++ src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/services/DIConnectionServiceTests.java
@@ -1,80 +1,100 @@
 package didentity.amos.digitalIdentity.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.http.ResponseEntity;
 
 import didentity.amos.digitalIdentity.enums.UserRole;
 import didentity.amos.digitalIdentity.model.User;
+import didentity.amos.digitalIdentity.repository.UserRepository;
 
 
 public class DIConnectionServiceTests {
 
+    @Mock
+    UserRepository userRepository;
+
+    @InjectMocks
+    DIConnectionService diConnectionService;
+
+    private User getDummyUser() {
+        User user = new User();
+        user.setId(1);
+        user.setName("Test");
+        user.setSurname("Test");
+        user.setEmail("test@test.test");
+        user.setUserRole(UserRole.EMPLOYEE);
+        return user;
+    }
+
     @Test
     public void testUpdateGeneric() {
        // Arrange
-       User expected = new User();
-       expected.setId(1);
-       expected.setName("TestChanged");
-       expected.setSurname("TestChanged");
-       expected.setEmail("test@test.test");
-       expected.setUserRole(UserRole.HR_EMPLOYEE);
-       DIConnectionService diConnectionService = new DIConnectionService();
-       diConnectionService.create("Test", "Test", "test@test.test", "employee");
+       User expected = getDummyUser();
+       when(userRepository.findById(1)).thenReturn(Optional.of(expected));
 
        // Act
        ResponseEntity<String> responseEntity = diConnectionService.update(1, "TestChanged", "TestChanged", "test@test.test", "hr_employee");
-
+       
        //Assert
+       
+       User changedUser = getDummyUser();
+       changedUser.setName("TestChanged");
+       changedUser.setSurname("TestChanged");
+       changedUser.setUserRole(UserRole.HR_EMPLOYEE);
+
+       ArgumentCaptor<User> argument = ArgumentCaptor.forClass(User.class);
+       verify(userRepository).save(argument.capture());
+       assertEquals(changedUser, argument.getValue());
+
        assertEquals(200, responseEntity.getStatusCode());
-       assertEquals(expected, diConnectionService.getConnectionById(1));
     }
 
     @Test
     public void testUpdateWrongID() {
        // Arrange
-       DIConnectionService diConnectionService = new DIConnectionService();
+       User expected = getDummyUser();
+       when(userRepository.findById(1)).thenReturn(Optional.of(expected));
 
        // Act
        ResponseEntity<String> responseEntity = diConnectionService.update(-1, "TestChanged", "TestChanged", "test@test.test", "hr_employee");
 
        //Assert
+       verify(userRepository, never()).save(any(User.class));
        assertEquals(400, responseEntity.getStatusCode());
     }
 
     @Test
     public void testUpdateWrongRole() {
         // Arrange
-        User expected = new User();
-        expected.setId(1);
-        expected.setName("Test");
-        expected.setSurname("Test");
-        expected.setEmail("test@test.test");
-        expected.setUserRole(UserRole.EMPLOYEE);
-        DIConnectionService diConnectionService = new DIConnectionService();
-        diConnectionService.create("Test", "Test", "test@test.test", "employee");
+        User expected = getDummyUser();
+        when(userRepository.findById(1)).thenReturn(Optional.of(expected));
 
         // Act
         ResponseEntity<String> responseEntity = diConnectionService.update(1, "TestChanged", "TestChanged", "test@test.test", "EEEEEEEEEEEEEEEEEEEEEE");
 
+        
         //Assert
+        verify(userRepository, never()).save(any(User.class));
         assertEquals(500, responseEntity.getStatusCode());
-        assertEquals(expected, diConnectionService.getConnectionById(1));
     }
 
 
     @Test
     public void testGetConnectionByIdGeneric() {
         // Arrange
-        User expected = new User();
-        expected.setId(1);
-        expected.setName("Test");
-        expected.setSurname("Test");
-        expected.setEmail("test@test.test");
-        expected.setUserRole(UserRole.EMPLOYEE);
-        DIConnectionService diConnectionService = new DIConnectionService();
-        diConnectionService.create("Test", "Test", "test@test.test", "employee");
+        User expected = getDummyUser();;
+        when(userRepository.findById(1)).thenReturn(Optional.of(expected));
 
         // Act
         User result = diConnectionService.getConnectionById(1);
@@ -87,8 +107,8 @@ public class DIConnectionServiceTests {
     public void testGetConnectionByIdWrong() {
         // Arrange
         User expected = null;
-        DIConnectionService diConnectionService = new DIConnectionService();
-        diConnectionService.create("Test", "Test", "test@test.test", "employee");
+        User user = getDummyUser();
+        when(userRepository.findById(1)).thenReturn(Optional.of(user));
 
         // Act
         User result = diConnectionService.getConnectionById(-1);
