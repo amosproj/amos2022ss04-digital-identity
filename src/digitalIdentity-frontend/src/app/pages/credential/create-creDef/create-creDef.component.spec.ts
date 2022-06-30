1872d7791c35f9d5a963750920316559ab23eb38
Steve Pegouen <steve.pegouen@fau.de>
2022-06-30 15:55:41 +0200

    Setup again the Testing file to fix some errors
    Signed-off-by: Steve Pegouen <steve.pegouen@fau.de>

--- src/digitalIdentity-frontend/src/app/pages/credential/create-creDef/create-creDef.component.spec.ts
+++ src/digitalIdentity-frontend/src/app/pages/credential/create-creDef/create-creDef.component.spec.ts
@@ -1,112 +1,80 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {HttpParams, HttpResponse} from '@angular/common/http';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {Router} from '@angular/router';
+import {RouterTestingModule} from '@angular/router/testing';
+import {MaterialModule} from 'src/app/components/material/material.module';
+import {BackendHttpService} from 'src/app/services/backend-http-service/backend-http-service.service';
 
-import { CreateCreDefComponent } from './create-creDef.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MaterialModule } from '../../../components/material/material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { By } from '@angular/platform-browser';
-import { BackendHttpService } from '../../../services/backend-http-service/backend-http-service.service';
-import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { RouterTestingModule } from '@angular/router/testing';
-import { DebugElement } from '@angular/core';
-import { LoginPageComponent } from '../../login-page/login-page.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+import {CreateCreDefComponent} from './create-creDef.component';
+import {By} from "@angular/platform-browser";
+import {CreateSchemaPageComponent} from "../../schema/create-schema-page/create-schema-page.component";
 
 describe('CreateCredentialComponent', () => {
   let component: CreateCreDefComponent;
   let fixture: ComponentFixture<CreateCreDefComponent>;
-  let de: DebugElement;
+  let httpService: BackendHttpService;
+  let router: Router;
+
+  let provided_router = {
+    navigate: jasmine.createSpy('navigate'),
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CreateCreDefComponent],
       imports: [
-        HttpClientTestingModule,
-        RouterTestingModule,
-        FormsModule,
-        ReactiveFormsModule,
-        MatDialogModule,
         MaterialModule,
-        BrowserAnimationsModule,
-      ],
-      providers: [HttpClient],
-    })
-      .compileComponents()
-      .then(() => {
-        fixture = TestBed.createComponent(CreateCreDefComponent);
-        component = fixture.componentInstance;
 
-        fixture.detectChanges();
-      });
+        BrowserAnimationsModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([
+          {path: '/credential-definition/create', component: CreateCreDefComponent},
+          {path: '/credDef-overview/'},
+        ]),
+      ],
+      providers: [
+        {
+          provide: Router,
+          useValue: provided_router,
+        },
+        // Router,
+        FormBuilder,
+        BackendHttpService,
+      ],
+    }).compileComponents();
   });
+
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateCreDefComponent);
     component = fixture.componentInstance;
-    de = fixture.debugElement;
+    httpService = TestBed.inject(BackendHttpService);
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   });
+
+  /*beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreateCreDefComponent ]
+    })
+      .compileComponents();
+  });*/
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateCreDefComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  /*
-  it('form invalid when empty', () => {
-    expect(component.credentialFormGroup.valid).toBeFalsy();
-  });
-
-  it('form should be valid with name', () => {
-    component.credentialFormGroup.controls['name'].setValue(generateRandomString(10));
-    expect(component.credentialFormGroup.valid).toBeTruthy();
-  });
-
-  it('should call initializer function in constructor', () => {
-    spyOn(component, 'getSchema').and.callFake(() => {});
-    TestBed.createComponent(CreateCreDefComponent);
-    expect(component.getSchema).toHaveBeenCalled();
-  });
-
-  it('Should have called init method after view init', () => {
-    spyOn(component, 'setInitialValue').and.callFake(() => {});
-    component.ngAfterViewInit();
-    expect(component.setInitialValue).toHaveBeenCalled();
-  });
-  it('should detect file input change and set uploadedFile', () => {
-    const dataTransfer = new DataTransfer()
-    dataTransfer.items.add(new File([''], 'test-file.pdf'))
-
-    const inputDebugEl  = fixture.debugElement.query(By.css('input[type=file]'));
-    inputDebugEl.nativeElement.files = dataTransfer.files;
-
-    inputDebugEl.nativeElement.dispatchEvent(new InputEvent('change'));
-
-    fixture.detectChanges();
-
-    expect(component.credentialFormGroup.controls['iconUrl']).toBeTruthy();
-    expect(component.credentialFormGroup.controls['iconUrl']).toBeInstanceOf(String);
-
-  });
-
-  it('file change event should arrive in handler', () => {
-    const element = fixture.nativeElement;
-    const input = element.querySelector('#file');
-    spyOn(component, 'selectFile');
-    input.dispatchEvent(new Event('change'));
-    fixture.detectChanges();
-    expect(component.selectFile).toHaveBeenCalled();
-  });
-
-  it('should create data when submitted', () => {
-    //expect(component.schemaData.length).toBeGreaterThan(0);
-    component.credentialFormGroup.controls['name'].setValue(generateRandomString(10));
-
-    let spy = spyOn(component, 'postCredential').and.callFake(function() {
-      component.createCredential();
-      expect(spy).toHaveBeenCalled();
-    });
-  });
-*/
 });
+
 function generateRandomString(length: number) {
   const characters = 'abcdefghijklmnopqrstuvwxyz';
   let result = '';
