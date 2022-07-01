6149e146f977b85c14697bc8adfbb976724234fa
Jannik Kiesel <jannik.kiesel@fau.de>
2022-07-01 18:18:01 +0200

    Added endpoint and connection to Lissi
    Signed-off-by: Jannik Kiesel <jannik.kiesel@fau.de>

---
+++ src/digitalIdentity-backend/src/main/java/didentity/amos/digitalIdentity/services/PresentationProofService.java
@@ -0,0 +1,24 @@
+package didentity.amos.digitalIdentity.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PresentationProofService {
+
+    @Autowired
+    private LissiApiService lissiApiService;
+
+    public ResponseEntity<String> sendProofTemplateToConnection(String connectionId, String proofTemplateId) {
+
+        ResponseEntity<String> response = lissiApiService
+                .sendProofTemplateToConnection(connectionId, proofTemplateId);
+
+        if (response == null) {
+            return ResponseEntity.status(500).body("Could not send proof request to connection.");
+        }
+        return ResponseEntity.status(201).body(response.getBody());
+    }
+    
+}
