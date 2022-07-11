383db481b4c598b6d7a4fd9573ec610a5cffffbf
Jean28518 <jean-frederic.vog.vogelbacher@fau.de>
2022-07-11 15:22:00 +0200

    Update special chars for password
    Signed-off-by: Jean28518 <jean-frederic.vog.vogelbacher@fau.de>

--- src/digitalIdentity-frontend/src/app/pages/change-password-page/change-password-page.component.ts
+++ src/digitalIdentity-frontend/src/app/pages/change-password-page/change-password-page.component.ts
@@ -150,6 +150,9 @@ export class ChangePasswordComponent implements OnInit {
     if (err['passwordStrengthNumeric']) {
       return 'numericError';
     }
+    if (err['disallowedChars']) {
+      return 'disallowedCharsError';
+    }
 
     return '';
   }
@@ -196,6 +199,13 @@ export function createPasswordStrengthValidator(): ValidatorFn {
     // has numeric
     if (!/[0-9]+/.test(value)) {
       return { passwordStrength: true, passwordStrengthNumeric: true };
+    }
+
+    // Only allow !@#$%^&* as special characters because of compatibilty issues between protocols
+    // Feel free to check it here: https://regexr.com/
+    var onlyAllowedCharacters = /^[a-zA-Z0-9!@#$%^&*]*$/;
+    if (!onlyAllowedCharacters.test(value)) {
+      return { passwordStrength: true, disallowedChars: true }
     }
 
     return null;
