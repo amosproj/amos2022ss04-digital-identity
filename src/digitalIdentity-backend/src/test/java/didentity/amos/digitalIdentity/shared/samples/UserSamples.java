5edccc0ca35ed6a896ae2ab7154b062756bda9b7
Jannik Kiesel <jannik.kiesel@fau.de>
2022-07-22 14:47:32 +0200

    #293 Changed back test classes to unencrypted
    Signed-off-by: Jannik Kiesel <jannik.kiesel@fau.de>

--- src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/shared/samples/UserSamples.java
+++ src/digitalIdentity-backend/src/test/java/didentity/amos/digitalIdentity/shared/samples/UserSamples.java
@@ -1,31 +1,22 @@
 package didentity.amos.digitalIdentity.shared.samples;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 import didentity.amos.digitalIdentity.enums.UserRole;
 import didentity.amos.digitalIdentity.model.User;
-import didentity.amos.digitalIdentity.services.EncryptionService;
 
 public class UserSamples {
 
-    @Autowired
-    private EncryptionService encryptionService;
-
-    public User getSampleUser() {
-        String passwordEncoded = encryptionService.encodeBase64("passwort123");
-        return getSampleUser(1, "achim@super.fix.com", "Achim", "Trautwein", passwordEncoded, UserRole.HR_EMPLOYEE,
+    public static User getSampleUser() {
+        return getSampleUser(1, "achim@super.fix.com", "Achim", "Trautwein", "passwort123", UserRole.HR_EMPLOYEE,
                 "invitationUrl", "connectionId");
     }
 
-    public User getSampleUser(Integer id, String email, String name, String surname, String password,
+    public static User getSampleUser(Integer id, String email, String name, String surname, String password,
             UserRole userRole, String invitationUrl, String connectionID) {
-        String passwordEncoded = encryptionService.encodeBase64(password);
 
         User user = new User();
         user.setEmail(email);
         user.setName(name);
         user.setSurname(surname);
-        user.setPassword(passwordEncoded);
+        user.setPassword(password);
         user.setUserRole(userRole);
         user.setConnectionId(connectionID);
         user.setInvitationUrl(invitationUrl);
