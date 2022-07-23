8306464eff878be9589e7496f1f12eddefaa8f55
Annika Krause <annika.krause@fau.de>
2022-07-23 23:49:32 +0200

    adjusted wording
    Signed-off-by: Annika Krause <annika.krause@fau.de>

--- src/digitalIdentity-frontend/src/app/services/error-message-service/error-message.service.ts
+++ src/digitalIdentity-frontend/src/app/services/error-message-service/error-message.service.ts
@@ -4,33 +4,34 @@ import { MatDialog } from '@angular/material/dialog';
 import { InformationPopUpComponent } from 'src/app/shared/pop-up/information-pop-up/information-pop-up.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ErrorMessageService {
+  private dialogOpen = false;
 
-  private dialogOpen = false
-
-  constructor(private matDialog: MatDialog) { }
+  constructor(private matDialog: MatDialog) {}
 
   openHttpErrorDialog(error: HttpErrorResponse) {
-
-    
     let data = {
       header: 'Process failed!',
       text: 'Error ' + error.status + ' \n' + error.error,
     };
-    
+
     if (error.status == 401) {
       data = {
         header: 'Login session expired (401)',
         text: 'Please log in to your account',
       };
     }
-    
+
     if (error.status >= 500) {
       data = {
         header: 'Internal Error (' + error.status + ')',
-        text: 'The server isn\'t configured right, is unavailable at the moment or a connected service isn\'t working right. This error message could help:\n\n\n' + error.message + '\n\n' + error.error,
+        text:
+          "The server isn't configured correctly, is unavailable at the moment or a connected service doesn't work properly. This error message could help:\n\n\n" +
+          error.message +
+          '\n\n' +
+          error.error,
       };
       if (typeof error.error === 'string' || error.error instanceof String) {
         if (error.error.length < 55) {
@@ -39,24 +40,25 @@ export class ErrorMessageService {
         }
       }
 
-      if (data.text.includes("Error occurred while trying to proxy:")) {
-        data.header = "Backend is not reachable.";
-        data.text = "Please contact your server administrator!\nIs the backend running? Is the backend reachable? Are the proxy settings correct?";
+      if (data.text.includes('Error occurred while trying to proxy:')) {
+        data.header = 'Backend is not reachable.';
+        data.text =
+          'Please contact your server administrator!\nIs the backend running? Is the backend reachable? Are the proxy settings correct?';
       }
     }
-    
+
     if (error.status == undefined) {
       data = {
         header: 'Internal Error',
-        text: 'The server isn\'t configured right or is unavailable at the moment.',
+        text: "The server isn't configured correctly or is unavailable at the moment.",
       };
     }
-    
+
     this.openDialog(data);
   }
-  
+
   openCustomErrorDialog(text = ``, header = `Process failed!`) {
-    this.openDialog( {
+    this.openDialog({
       header: header,
       text: text,
     });
@@ -64,7 +66,9 @@ export class ErrorMessageService {
 
   private openDialog(data: any) {
     if (this.dialogOpen) {
-      console.log("Another message should be displayed, but is rejected because another error message is still open. It will be displayed here:");
+      console.log(
+        'Another message should be displayed, but is rejected because another error message is still open. It will be displayed here: '
+      );
       console.log(data);
       return;
     }
@@ -75,7 +79,6 @@ export class ErrorMessageService {
     });
     this.matDialog.afterAllClosed.subscribe(() => {
       this.dialogOpen = false;
-    })
+    });
   }
-
 }
